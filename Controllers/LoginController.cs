d4a9226ac1895452b67376289bb0f34d29e7b055
r.rajaeerad <r.rajaeerad@dotin.ir>
2024-01-16 11:21:35 +0330

    change to dev

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CustomerSurveySystem.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET
+        public ActionResult Login()
+        {
+            //return View();
+            return null;
+        }
+    }
+}
