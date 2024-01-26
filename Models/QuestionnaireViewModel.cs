29997133a6f4d4efcfdb466112687669034bd6bb
r.rajaeerad <r.rajaeerad@dotin.ir>
2024-01-26 12:31:51 +0330

    Add ViewModel

---
+++ Models/QuestionnaireViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerSurveySystem.Models
+{
+    public class QuestionnaireViewModel
+    {
+        public Guid QuestionnaireId { get; set; }
+        public string QuestionnaireTitle { get; set; }
+        public Guid AnswerSheetId { get; set; }
+        public IList<QuestionDto> Questions { get; set; }
+    }
+}
